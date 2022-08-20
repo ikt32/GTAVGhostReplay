9a6e288088e20f3fb1e3e9ea7c32709b602aff76
E66666666 <6639510+E66666666@users.noreply.github.com>
2022-08-20 22:18:30 +0200

    Set version to 2.3.1

--- GhostReplay/Constants.hpp
+++ GhostReplay/Constants.hpp
@@ -4,10 +4,10 @@
 
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 3
-#define VERSION_PATCH 0
+#define VERSION_PATCH 1
 
 namespace Constants {
     static const char* const DisplayVersion = "v" STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH);
     static const char* const ModDir = "\\GhostReplay";
-    static const char* const NotificationPrefix = "~b~GhostReplay~w~";
+    static const char* const NotificationPrefix = "~b~Ghost Replay~w~";
 }
